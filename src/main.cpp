8a9cfea43accdbfa3f9a79690d47410f92df5c01
YarikCompany2 <yaroslavromanenko456@gmail.com>
2026-02-17 19:53:43 +0200

    feat: add cmake files to download and link all necessary libraries

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <glm/glm.hpp>
+#include <entt/entt.hpp>
+#include <assimp/aabb.h>
+
+int main() {
+    glfwInit();
+
+    std::cout << "Hello, World!" << std::endl;
+
+    glfwTerminate();
+    return 0;
+}
