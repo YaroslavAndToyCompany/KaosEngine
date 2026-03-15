ccc390ff83725f9a6bf135e640ae52a7c22489fc
toysmbb <tvinktoya@gmail.com>
2026-03-15 17:11:51 +0200

    feat: added Renderer class

--- include/ECS/System.h
+++ include/ECS/System.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <entt/entt.hpp>
+#include "ECS/World.h"
 
 
 class System 
---
+++ include/Renderer/Renderer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "ECS/Components.h"
+#include <glm/glm.hpp>
+
+class Renderer 
+{
+public:
+	static void init();
+	static void beginScene();
+	static void endScene();
+
+	//primitives
+	static void drawQuad(glm::vec2& position, glm::vec2& size, glm::vec4& color);
+	static void drawRectangle(glm::vec2& position, glm::vec2& size, glm::vec4& color);
+	static void DrawCircle(glm::vec2& position, glm::vec2& radius, glm::vec4& color);
+private:
+
+};
