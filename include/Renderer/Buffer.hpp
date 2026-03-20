dc34904b12938a0fb676abfedd1c0cf9a9e42ff7
YarikCompany2 <yaroslavromanenko456@gmail.com>
2026-03-20 20:06:33 +0200

    chore: change explicit byte count to sizeof() in Buffer dataTypeSize()

--- include/Renderer/Buffer.hpp
+++ include/Renderer/Buffer.hpp
@@ -15,17 +15,17 @@ static uint32_t dataTypeSize(ShaderType type)
 {
 	switch(type)
 	{
-	case ShaderType::Float:    return 4;
-	case ShaderType::Float2:   return 4 * 2;
-	case ShaderType::Float3:   return 4 * 3;
-	case ShaderType::Float4:   return 4 * 4;
-	case ShaderType::Mat3:     return 4 * 3 * 3;
-	case ShaderType::Mat4:     return 4 * 4 * 4;
-	case ShaderType::Int:      return 4;
-	case ShaderType::Int2:     return 4 * 2;
-	case ShaderType::Int3:     return 4 * 3;
-	case ShaderType::Int4:     return 4 * 4;
-	case ShaderType::Bool:     return 1;
+	case ShaderType::Float:    return sizeof(float);
+	case ShaderType::Float2:   return sizeof(float) * 2;
+	case ShaderType::Float3:   return sizeof(float) * 3;
+	case ShaderType::Float4:   return sizeof(float) * 4;
+	case ShaderType::Mat3:     return sizeof(float) * 3 * 3;
+	case ShaderType::Mat4:     return sizeof(float) * 4 * 4;
+	case ShaderType::Int:      return sizeof(int);
+	case ShaderType::Int2:     return sizeof(int) * 2;
+	case ShaderType::Int3:     return sizeof(int) * 3;
+	case ShaderType::Int4:     return sizeof(int) * 4;
+	case ShaderType::Bool:     return sizeof(bool);
 	}
 	return 0;
 }
