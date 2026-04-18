2ece0ca459de7d38ceac4c612c0354cfaf2aaf65
YarikCompany2 <yaroslavromanenko456@gmail.com>
2026-04-18 19:42:11 +0300

    refactor: change some functions in SizeBasedArena

---
+++ include/Memory/SizeBasedArena.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Utils/Logger.hpp"
+#include <cstddef>
+#include <cstdlib>
+#include <new>
+
+template <class T, std::size_t N>
+class SizeBasedArena
+{
+public:
+    SizeBasedArena();
+    ~SizeBasedArena();
+
+    void* allocateOne();
+    void* allocateArray(std::size_t);
+    void deallocateOne(void*) noexcept {}
+    void deallocateArray(void*) noexcept {}
+
+private:
+    char* m_basePtr;
+    char* m_curPtrPos;
+    
+    SizeBasedArena(const SizeBasedArena&) = delete;
+    SizeBasedArena& operator=(const SizeBasedArena&) = delete;
+
+    static constexpr const char* m_className = "ArenaBasedAlloc";
+};
+
+template <typename T, std::size_t N>
+SizeBasedArena<T, N>::SizeBasedArena()
+    : m_basePtr(static_cast<char*>(std::malloc(N * sizeof(T))))
+{
+    if (!m_basePtr) throw std::bad_alloc();
+    m_curPtrPos = m_basePtr;
+
+    Logger::get().log(m_className, "Memory is allocated");
+}
+
+template <class T, std::size_t N>
+SizeBasedArena<T, N>::~SizeBasedArena()
+{
+    std::free(m_basePtr);
+    Logger::get().log(m_className, "Memory is deallocated");
+}
+
+template <typename T, std::size_t N>
+void* SizeBasedArena<T, N>::allocateOne()
+{
+    char* memToReturn = m_curPtrPos;
+    m_curPtrPos += sizeof(T);
+    Logger::get().log(m_className, "Memory is given for one object");
+
+    return memToReturn;
+}
+
+template <typename T, std::size_t N>
+void* SizeBasedArena<T, N>::allocateArray(std::size_t n)
+{
+    char* memToReturn = m_curPtrPos;
+    m_curPtrPos += n * sizeof(T);
+    Logger::get().log(m_className, "Memory is given for array of objects");
+
+    return memToReturn;
+}
