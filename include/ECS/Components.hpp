499cb7582a9c635ca05ff640a118e2bf4c3e223d
toysmbb <tvinktoya@gmail.com>
2026-07-14 21:23:42 +0300

    feat: add meshcomponent

--- include/ECS/Components.hpp
+++ include/ECS/Components.hpp
@@ -1,6 +1,15 @@
 #pragma once
+#include "Renderer/VertexArray.hpp"
+#include "Utils/SlotMap.hpp"
+#include <memory>
 
 struct TransformComponent
 {
 	float transform;
+};
+
+struct MeshComponent
+{
+    std::shared_ptr<VertexArray> vertexArray;
+    AssetHandle shaderHandle;
 };
